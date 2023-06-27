9639036f18db4d2eb80ca68d28eeb777fea9fea0
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2023-06-27 18:19:50 -0300

    FEAT(leitura hidrica)

--- sustenApp/main.cpp
+++ sustenApp/main.cpp
@@ -3,10 +3,15 @@
 #include <Ultrasonic.h>
 #include <EmonLib.h>
 
-const int TX = 0, RX = 1, HIDROMETRO = 3, SCT = 4, RESERVATORIO_ECHO = 5, RESERVATORIO_TRIGGER = 6; 
-const int DISPOSTIVO_01 = 10, DISPOSTIVO_02 = 11, DISPOSTIVO_03 = 12; 
+const int TX = 0, RX = 1, HIDROMETRO = 3, SCT = 4, RESERVATORIO_ECHO = 5, RESERVATORIO_TRIGGER = 6;
+const int DISPOSTIVO_01 = 10, DISPOSTIVO_02 = 11, DISPOSTIVO_03 = 12;
 const int LIMITE = 13, CAPACITY = 96, TENSAO = 110;
-double ALTURA_RESERVATORIO = 0, KWH = 0;
+const int INTERRUPCAO_AGUA = 0;
+const float FATOR_CALIBRACAO = 4.5;
+
+double ALTURA_RESERVATORIO = 0, KWH = 0, CONTADOR_AGUA = 0, FLUXO = 0, VOLUME = 0, VOLUME_TOTAL = 0;
+int CONTADOR_AGUA = 0;
+unsigned long ULTIMA_EXECUCAO = 0;
 
 const int portasSaida[LIMITE] = {DISPOSTIVO_01};
 const int portasEntrada[LIMITE] = {};
@@ -14,11 +19,12 @@ const int portasEntrada[LIMITE] = {};
 SoftwareSerial bluetooth(RX, TX);
 Ultrasonic ultrasonic(RESERVATORIO_TRIGGER, RESERVATORIO_ECHO);
 EnergyMonitor energia;
-StaticJsonDocument<CAPACITY> JSON;
+StaticJsonDocument <CAPACITY> JSON;
 
 void setup() {
     declaraPortasDeSaida();
     declaraPortasDeEntrada();
+    pinMode(HIDROMETRO, INPUT_PULLUP);
     bluetooth.begin(9600);
     //energia.current(SCT, 0.0607); //EnergyMonitor SCT013
 }
@@ -30,8 +36,8 @@ void loop() {
 }
 
 void declaraPortasDeEntrada() {
-    for(int i = 0; i < LIMITE; i++) {
-        if(!portasEntrada[i]) {
+    for (int i = 0; i < LIMITE; i++) {
+        if (!portasEntrada[i]) {
             break;
         }
 
@@ -40,8 +46,8 @@ void declaraPortasDeEntrada() {
 }
 
 void declaraPortasDeSaida() {
-    for(int i = 0; i < LIMITE; i++) {
-        if(!portasSaida[i]) {
+    for (int i = 0; i < LIMITE; i++) {
+        if (!portasSaida[i]) {
             break;
         }
 
@@ -53,31 +59,31 @@ void declaraPortasDeSaida() {
 // LEITURA CONTINUA 
 
 void leituraBluetooth() {
-    if(bluetooth.available()) {
-        while(bluetooth.available()){
+    if (bluetooth.available()) {
+        while (bluetooth.available()) {
             descompactaJSON();
 
-            if(retornaStringJSON("comando") == "consumo") {
-                if(retornaStringJSON("tipo") == "hidrico") {
-                    if(retornaBoolJSON("renovavel")) {
+            if (retornaStringJSON("comando") == "consumo") {
+                if (retornaStringJSON("tipo") == "hidrico") {
+                    if (retornaBoolJSON("renovavel")) {
                         enviaRelatorio(retornaVolumeReservatorio());
-                    } else{
+                    } else {
                         enviaRelatorio(retornaConsumoHidrico());
                     }
-                } else if(retornaStringJSON("tipo") == "eletrico") {
-                    if(retornaBoolJSON("renovavel")) {
+                } else if (retornaStringJSON("tipo") == "eletrico") {
+                    if (retornaBoolJSON("renovavel")) {
                         enviaRelatorio(retornaVolumePainelSolar());
-                    } else{
+                    } else {
                         enviaRelatorio(retornaConsumoEletrico());
                     }
                 }
-            } else if(retornaStringJSON("comando") == "controlador") {
+            } else if (retornaStringJSON("comando") == "controlador") {
                 controlaDispositivo(retornaIntJSON("porta"));
-            } else if(retornaStringJSON("comando") == "dispositivo") {
+            } else if (retornaStringJSON("comando") == "dispositivo") {
                 JSON["estado"] = retornaEstadoDispositivo(retornaIntJSON("porta"));
                 enviaInformacaoBluetooth(compactaJSON());
-            } else if(retornaStringJSON("comando") == "declaracao") {
-                if(retornaStringJSON("tipo") == "reservatorio") {
+            } else if (retornaStringJSON("comando") == "declaracao") {
+                if (retornaStringJSON("tipo") == "reservatorio") {
                     declaraAlturaReservatorio();
                 }
             } else {
@@ -88,17 +94,27 @@ void leituraBluetooth() {
 }
 
 void leituraEletrica() {
-    double IRMS = energia.calcIrms(1480); // Calcula o valor da Corrente, usando 1480 amostras
+    double IRMS = energia.calcIrms(1480); 
 
-    if(IRMS < 0.02) {
+    if (IRMS < 0.02) {
         return;
-    } 
+    }
 
-    KWH += (IRMS * TENSAO)/1000;
+    KWH += (IRMS * TENSAO) / 1000;
 }
 
 void leituraHidrica() {
+    if ((millis() - ULTIMA_EXECUCAO) > 1000) {
+        detachInterrupt(INTERRUPCAO_SENSOR);
 
+        fluxo = ((1000.0 / (millis() - ULTIMA_EXECUCAO)) * CONTADOR_AGUA) / FATOR_CALIBRACAO;
+        VOLUME = FLUXO / 60;
+        VOLUME_TOTAL += VOLUME;
+        CONTADOR_AGUA = 0;
+        ULTIMA_EXECUCAO = millis();
+
+        attachInterrupt(INTERRUPCAO_SENSOR, incrementaContador, FALLING);
+    }
 }
 
 // COMUNICACAO
@@ -115,12 +131,12 @@ void enviaInformacaoBluetooth(String informacao) {
 
 void descompactaJSON() {
     String request = recebeInformacaoBluetooth();
-    
+
     DeserializationError error = deserializeJson(JSON, request);
-    if(error != DeserializationError::Ok) {
+    if (error != DeserializationError::Ok) {
         enviaInformacaoBluetooth(error.c_str());
     }
-} 
+}
 
 String compactaJSON() {
     String resposta;
@@ -136,25 +152,29 @@ int retornaEstadoDispositivo(int porta) {
 }
 
 String retornaStringJSON(String atributo) {
-    return JSON[atributo].as<String>();
+    return JSON[atributo].as < String > ();
 }
 
 int retornaIntJSON(String atributo) {
-    return JSON[atributo].as<int>();
+    return JSON[atributo].as < int > ();
 }
 
 double retornaDoubleJSON(String atributo) {
-    return JSON[atributo].as<double>();
+    return JSON[atributo].as < double > ();
 }
 
 bool retornaBoolJSON(String atributo) {
-    return JSON[atributo].as<bool>();
+    return JSON[atributo].as < bool > ();
 }
 
 // CONTROLADOR
 
 void controlaDispositivo(int porta) {
     digitalWrite(porta, !retornaEstadoDispositivo(porta));
+}
+
+void incrementaContador() {
+    CONTADOR_AGUA++;
 }
 
 // RENOVAVEL
