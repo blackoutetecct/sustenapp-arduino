962e12f0b386fed41c6f8eb44b1cecd7f68bf37d
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2023-07-29 23:30:31 -0300

    Update main.cpp

--- sustenApp/main.cpp
+++ sustenApp/main.cpp
@@ -1,47 +1,62 @@
-#include <SoftwareSerial.h>
+#include "BluetoothSerial.h"
 #include <ArduinoJson.h>
 #include <Ultrasonic.h>
 #include <EmonLib.h>
 
-const int TX = 0, RX = 1, HIDROMETRO = 3, SCT = A0, RESERVATORIO_ECHO = 5, RESERVATORIO_TRIGGER = 6;
-const int LIMITE = 13, CAPACITY = 96, TENSAO = 110;
+/* BLUETOOTH */
 
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled!Please run `make menuconfig`to and enable it
+#endif
+
+/* PORTAS */
+
+const int SCT = 21, RESERVATORIO_ECHO = 22, RESERVATORIO_TRIGGER = 23;
+int portasDisponiveis[18] = {2,4,5,12,13,14,15,25,26,27,32,33,34,35,36,39,18,19};
+
+/* VARIAVEIS */ 
+
+const int CAPACITY_JSON = 96, TENSAO = 110, CALIBRACAO = 0.9;
 double ALTURA_RESERVATORIO = 0, CAPACIDADE_RESERVATORIO = 0, KWH = 0;
 
-// AGUA
+/* INSTANCIACAO */ 
 
-const float FATOR_CALIBRACAO = 7.5;
-const int INTERRUPCAO_SENSOR = 0;
-
-double CONTADOR_AGUA = 0, VOLUME_TOTAL = 0;
-unsigned long ULTIMA_EXECUCAO = 0;
-
-// -----
-
-int portasSaida[LIMITE] = {};
-int portasEntrada[LIMITE] = {};
-int portasDispositivo[4] = {10, 11, 12, 13};
-
-SoftwareSerial bluetooth(RX, TX);
+BluetoothSerial bluetooth;
 Ultrasonic ultrasonic(RESERVATORIO_TRIGGER, RESERVATORIO_ECHO);
 EnergyMonitor energia;
-StaticJsonDocument <CAPACITY> JSON;
+StaticJsonDocument<CAPACITY_JSON>JSON;
+
+TaskHandle_t TaskBluetooth;
+TaskHandle_t TaskEletrica;
 
 void setup() {
-    declaraPortasDeSaida();
-    declaraPortasDeEntrada();
-    pinMode(HIDROMETRO, INPUT_PULLUP);
-    bluetooth.begin(9600);
-    energia.current(SCT, 60); 
+    Serial.begin(9600);
+    bluetooth.begin("SUSTENAPP - CENTRAL");
+    energia.current(SCT, CALIBRACAO);
+    
+    xTaskCreatePinnedToCore(taskBluetooth, "TaskBluetooth", 10000, NULL, 1, &TaskBluetooth, 0);
+    xTaskCreatePinnedToCore(taskEletrica, "TaskEletrica", 10000, NULL, 1, &TaskEletrica, 1);
 }
 
 void loop() {
-    leituraBluetooth();
-    leituraEletrica();
-    leituraHidrica();
+
 }
 
-// LEITURA CONTINUA 
+/* ASSINCRONISMO */
+
+void taskBluetooth(void * pvParameters) {
+    for (;;) {
+        leituraBluetooth();
+    }
+}
+
+void taskEletrica(void * pvParameters) {
+    for (;;) {
+        leituraEletrica();
+    }
+}
+
+/* LEITURA CONTINUA/ASSINCRONA */
 
 void leituraBluetooth() {
     if (bluetooth.available()) {
@@ -53,7 +68,7 @@ void leituraBluetooth() {
                     if (retornaBoolJSON("renovavel")) {
                         enviaRelatorio(retornaVolumeReservatorio(), "L");
                     } else {
-                        enviaRelatorio(retornaConsumoHidrico(), "M3");
+                        //enviaRelatorio(retornaConsumoHidrico(), "M3");
                     }
                 } else if (retornaStringJSON("tipo") == "eletrico") {
                     enviaRelatorio(retornaConsumoEletrico(), "KW/H");
@@ -76,7 +91,7 @@ void leituraBluetooth() {
 }
 
 void leituraEletrica() {
-    double IRMS = energia.calcIrms(1480); 
+    double IRMS = energia.calcIrms(1480);
 
     if (IRMS < 0.02) {
         return;
@@ -85,34 +100,20 @@ void leituraEletrica() {
     KWH += (IRMS * TENSAO) / 1000;
 }
 
-void leituraHidrica() {
-    unsigned long INTERVALO = millis() - ULTIMA_EXECUCAO;
-
-    if (INTERVALO > 1000) {
-        detachInterrupt(INTERRUPCAO_SENSOR);
-
-        double FLUXO = ((1000.0 / INTERVALO) * CONTADOR_AGUA) / FATOR_CALIBRACAO;
-        double VOLUME = FLUXO / 60;
-
-        VOLUME_TOTAL += VOLUME / 100; //(VOLUME / 1000);
-        CONTADOR_AGUA = 0;
-        ULTIMA_EXECUCAO = millis();
-
-        attachInterrupt(INTERRUPCAO_SENSOR, incrementaContador, FALLING);
-    }
-}
-
-// COMUNICACAO
+/* COMUNICACAO BLUETOOTH */
 
 String recebeInformacaoBluetooth() {
-    return String(bluetooth.readString());
+    return bluetooth.readString();
 }
 
 void enviaInformacaoBluetooth(String informacao) {
-    bluetooth.println(adicionaChaves(informacao));
+    String JSON_AUXILIAR = "{" + informacao + "}";
+    char RETURN[JSON_AUXILIAR.length()];
+    memcpy(RETURN, JSON_AUXILIAR.c_str(), JSON_AUXILIAR.length());
+    bluetooth.write((uint8_t * ) RETURN, JSON_AUXILIAR.length());
 }
 
-// -> CONVERSAO
+/* CONVERSAO JSON */
 
 void descompactaJSON() {
     String request = recebeInformacaoBluetooth();
@@ -130,50 +131,38 @@ String compactaJSON() {
     return resposta;
 }
 
-// -> EXTRACAO DE INFORMACAO
+/* EXTRACAO DE VALORES JSON */
 
 String retornaStringJSON(String atributo) {
-    return JSON[atributo].as<String>();
+    return JSON[atributo].as < String > ();
 }
 
 int retornaIntJSON(String atributo) {
-    return JSON[atributo].as <int> ();
+    return JSON[atributo].as < int > ();
 }
 
 double retornaDoubleJSON(String atributo) {
-    return JSON[atributo].as<double>();
+    return JSON[atributo].as < double > ();
 }
 
 bool retornaBoolJSON(String atributo) {
-    return JSON[atributo].as<bool>();
+    return JSON[atributo].as < bool > ();
 }
 
-// CONTROLADOR
+/* CONTROLADOR DE DISPOSITIVO */
 
 void controlaDispositivo(int porta) {
     int estado = !retornaEstadoDispositivo(porta);
     digitalWrite(porta, estado);
-    enviaEstadoDispositivo(200, estado);
+    enviaEstadoDispositivo(200, !estado);
 }
 
-// INCREMENTADOR
-
-void incrementaContador() {
-    CONTADOR_AGUA++;
-}
-
-// STATUS
+/* STATUS */
 
 double retornaConsumoEletrico() {
     double KWH_RETURN = KWH;
     KWH = 0;
     return KWH_RETURN;
-}
-
-double retornaConsumoHidrico() {
-    double VOLUME_TOTAL_RETURN = VOLUME_TOTAL;
-    VOLUME_TOTAL = 0;
-    return VOLUME_TOTAL_RETURN;
 }
 
 double retornaVolumeReservatorio() {
@@ -184,27 +173,7 @@ int retornaEstadoDispositivo(int porta) {
     return digitalRead(porta);
 }
 
-// DECLARACAO
-
-void declaraPortasDeEntrada() {
-    for (int i = 0; i < LIMITE; i++) {
-        if (!portasEntrada[i]) {
-            break;
-        }
-
-        declaraPorta(portasEntrada[i], INPUT);
-    }
-}
-
-void declaraPortasDeSaida() {
-    for (int i = 0; i < LIMITE; i++) {
-        if (!portasSaida[i]) {
-            break;
-        }
-
-        declaraPorta(portasSaida[i], OUTPUT);
-    }
-}
+/* DECLARACAO */
 
 void declaraReservatorio(double capacidade) {
     CAPACIDADE_RESERVATORIO = capacidade; // L
@@ -213,10 +182,12 @@ void declaraReservatorio(double capacidade) {
     enviaStatus(200, "RESERVATORIO CONFIGURADO");
 }
 
+/* GERENCIAMENTO DE PORTAS */
+
 void declaracaoDispositivo(int porta) {
-    for (int i = 0; i < LIMITE; i++) {
-        if (!portasSaida[i]) {
-            portasSaida[i] = porta;
+    for (int i = 0; i < 18; i++) {
+        if (portasDisponiveis[i] == porta) {
+            portasDisponiveis[i] = NULL;
             declaraPorta(porta, OUTPUT);
             enviaStatus(200, "DISPOSITIVO ADICIONADO");
             return;
@@ -234,13 +205,13 @@ void declaraPorta(int porta, int fluxo) {
     }
 }
 
-// RELATORIO
+/* RELATORIO */
 
 void enviaRelatorio(double consumo, String unidade) {
     enviaInformacaoBluetooth(
         "'consumo':" + String(consumo) + ",'unidade':'" + unidade + "'"
     );
-} 
+}
 
 void enviaStatus(int status, String mensagem) {
     enviaInformacaoBluetooth(
@@ -252,8 +223,4 @@ void enviaEstadoDispositivo(int status, int estado) {
     enviaInformacaoBluetooth(
         "'status':" + String(status) + ",'estado':" + String(estado)
     );
-}
-
-String adicionaChaves(String JSON) {
-    return "{" + JSON + "}";
 }
