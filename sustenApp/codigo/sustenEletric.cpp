0cbcc04e56705e67c2a16ed92388f9fdb116e261
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2023-11-28 18:36:09 -0300

    Update sustenEletric.cpp

--- sustenApp/codigo/sustenEletric.cpp
+++ sustenApp/codigo/sustenEletric.cpp
@@ -1,8 +1,8 @@
 #include <EmonLib.h>
 #include <HardwareSerial.h>
 
-const int SENSOR = 2, TENSAO = 110, CALIBRACAO = 6.0606, LED_ACIONAMENTO = 23;
-double W;
+const int SENSOR = 2, TENSAO = 110, CALIBRACAO = 6.0607, LED_ACIONAMENTO = 23;
+double WH;
 
 HardwareSerial SerialPort(2);
 EnergyMonitor energia;
@@ -18,22 +18,21 @@ void setup() {
 }
 
 void loop() {
-    SerialPort.println(getLeitura());
     leituraEletrica();
+    SerialPort.println(getLeitura());
+    delay(1000);
 }
 
 void leituraEletrica() {
     double IRMS = energia.calcIrms(1480);
-    energia.calcVI(20, 2000);
 
-    if (IRMS < 0.02 ||  energia.Vrms < 90) {
+    if (IRMS < 0.02) {
         return;
     }
 
-    W += 9.6; //(IRMS * TENSAO);
-    delay(1000);
+    WH += (IRMS * TENSAO) / 1000;
 }
 
 double getLeitura() {
-    return W;
+    return WH;
 }
