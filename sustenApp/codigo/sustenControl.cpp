9cf441ae8d6fa83de5d381b8caf7c7c508e188a0
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2023-12-05 06:51:36 -0300

    Update sustenControl.cpp

--- sustenApp/codigo/sustenControl.cpp
+++ sustenApp/codigo/sustenControl.cpp
@@ -2,17 +2,14 @@
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 #include <AsyncJson.h>
+#include <ArduinoJson.h>
 #include <Ultrasonic.h>
 #include <HardwareSerial.h>
 
 /* WIFI */
 
-const char * ssid = "REDE WIFI";
+const char * ssid = "REDE";
 const char * password = "SENHA";
-
-IPAddress staticIP(192, 168, 18, 226);
-IPAddress gateway(192, 168, 18, 1); 
-IPAddress subnet(255, 255, 255, 0); 
 
 AsyncWebServer server(80); 
 AsyncWebServerRequest * requestStatic;
@@ -29,45 +26,66 @@ int portasDisponiveis[18] = {4,5,12,13,14,15,25,26,27,32,33,34,35,36,39,18,19};
 
 /* VARIAVEIS */
 
+const int CAPACITY_JSON = 96;
 long double ALTURA_RESERVATORIO = 0, CAPACIDADE_RESERVATORIO = 0, KW = 0;
 
 /* INSTANCIACAO */
 
 Ultrasonic ultrasonic(RESERVATORIO_TRIGGER, RESERVATORIO_ECHO);
+StaticJsonDocument < CAPACITY_JSON > JSON;
 const int LED_ACIONAMENTO = 2;
 
 /* CONTROLE DE LEITURA */
 
 double ULTIMA_LEITURA_HIDRICA = 0, ULTIMA_LEITURA_ELETRICA = 0;
 
-void setup() {
-    inicializaWifi();
+void handleRequest(AsyncWebServerRequest * request) {}
 
-    /* SERIAL */
-    Serial.begin(115200);
-    serial_hidrico.begin(9600, SERIAL_8N1, 1, 3);
-    serial_eletrico.begin(4800, SERIAL_8N1, 16, 17);
+/* COMUNICACAO EXTERNA */
 
-    /* LED ACIONAMENTO */
-    pinMode(LED_ACIONAMENTO, OUTPUT);
-    digitalWrite(LED_ACIONEMENTO, HIGH);
+void enviaInformacao(String informacao) {
+	Serial.println(200);
+	Serial.println("application/json");
+	Serial.println(informacao);
+    requestStatic->send(200, "application/json", informacao);
 }
 
-void loop() {
-    contadorEletrico();
-    delay(1000);
+/* RELATORIO */
+
+void enviaRelatorio(double consumo, String unidade) {
+    enviaInformacao(
+        "{\"consumo\":" + String(consumo) + ",\"unidade\":\"" + unidade + "\"}"
+    );
 }
 
-/* COMUNICACAO SERIAL */
+void enviaStatus(int status, String mensagem) {
+    enviaInformacao(
+        "{\"status\":" + String(status) + ",\"mensagem\":\"" + mensagem + "\"}"
+    );
+}
 
-double recebeInformacaoSerial(String tipo) {
-    if (tipo == "hidrico") {
-        recebeInformacaoHidrico();
-    } else if (tipo == "eletrico") {
-        recebeInformacaoEletrico();
-    }
+void enviaEstadoDispositivo(int status, int estado) {
+    enviaInformacao(
+        "{\"status\":" + String(status) + ",\"estado\":" + String(estado) + "}"
+    );
+}
 
-    return 0.0;
+void enviaPortaDisponivel(int status, int porta) {
+    enviaInformacao(
+        "{\"status\":" + String(status) + ",\"porta\":" + String(porta) + "}"
+    );
+}
+
+int retornaEstadoDispositivo(int porta) {
+    return digitalRead(porta);
+}
+
+/* CONTROLADOR DE DISPOSITIVO */
+
+void controlaDispositivo(int porta) {
+    int estado = !retornaEstadoDispositivo(porta);
+    digitalWrite(porta, estado);
+    enviaEstadoDispositivo(200, !estado);
 }
 
 double recebeInformacaoHidrico() {
@@ -80,10 +98,6 @@ double recebeInformacaoHidrico() {
     }
 
     enviaStatus(400, "FALHA NA OBTENCAO DE CONSUMO");
-}
-
-void contadorEletrico() {
-    ULTIMA_LEITURA_ELETRICA += 9.6;
 }
 
 double recebeInformacaoEletrico() {
@@ -100,13 +114,87 @@ double recebeInformacaoEletrico() {
     enviaStatus(400, "FALHA NA OBTENCAO DE CONSUMO");
 }
 
-/* COMUNICACAO EXTERNA */
+/* COMUNICACAO SERIAL */
 
-void enviaInformacao(String informacao) {
-    requestStatic->send(200, "application/json", informacao);
+double recebeInformacaoSerial(String tipo) {
+    if (tipo == "hidrico") {
+        recebeInformacaoHidrico();
+    } else if (tipo == "eletrico") {
+        recebeInformacaoEletrico();
+    }
+
+    return 0.0;
 }
 
-/* WIFI */
+/* STATUS */
+
+double retornaConsumoEletrico() {
+    return recebeInformacaoSerial("eletrico");
+}
+
+double retornaConsumoHidrico() {
+    return recebeInformacaoSerial("hidrico");
+}
+
+double retornaVolumeReservatorio() {
+    return ((CAPACIDADE_RESERVATORIO / ALTURA_RESERVATORIO) * (ultrasonic.read() * 100) / ALTURA_RESERVATORIO);
+}
+
+
+
+/* DECLARACAO */
+
+void declaraReservatorio(double capacidade) {
+    CAPACIDADE_RESERVATORIO = capacidade; // L
+    ALTURA_RESERVATORIO = ultrasonic.read(); // CM
+
+    enviaStatus(200, "RESERVATORIO CONFIGURADO");
+}
+
+
+void declaraEletricidade() {
+  enviaStatus(200, "ELETRICIDADE MONITORAVEL");
+}
+
+void declaraHidricidade() {
+  enviaStatus(200, "ELETRICIDADE MONITORAVEL");
+}
+
+/* GERENCIAMENTO DE PORTAS */
+
+void informaPortaDisponivel() {
+    for (int i = 0; i < 18; i++) {
+        if (portasDisponiveis[i]) {
+            enviaPortaDisponivel(200, portasDisponiveis[i]);
+            return;
+        }
+    }
+
+    enviaStatus(200, "ALCANCE DE LIMITE DE PORTAS UTILIZAVEIS");
+}
+
+void declaraPorta(int porta, int fluxo) {
+    pinMode(porta, fluxo);
+
+    if (fluxo == OUTPUT) {
+        digitalWrite(porta, LOW);
+    }
+}
+
+void declaracaoDispositivo(int porta) {
+    for (int i = 0; i < 18; i++) {
+        if (portasDisponiveis[i] == porta) {
+            portasDisponiveis[i] = NULL;
+            declaraPorta(porta, OUTPUT);
+            enviaStatus(200, "DISPOSITIVO ADICIONADO");
+            return;
+        }
+    }
+
+    enviaStatus(402, "FALHA NA ADICAO DO DISPOSITIVO");
+}
+
+
 
 AsyncCallbackJsonWebHandler * handle = new AsyncCallbackJsonWebHandler(
     "/", [](AsyncWebServerRequest * request, JsonVariant & jsonReceiver) {
@@ -144,121 +232,37 @@ AsyncCallbackJsonWebHandler * handle = new AsyncCallbackJsonWebHandler(
     }
 );
 
-void handleRequest(AsyncWebServerRequest * request) {}
- 
 void inicializaWifi() {
-    WiFi.config(staticIP, gateway, subnet);
     WiFi.begin(ssid, password);
 
     while (WiFi.status() != WL_CONNECTED) {
-        delay(1000);
+        delay(100);
     }
+
+    Serial.print("IP: ");
+    Serial.println(WiFi.localIP());
 
     server.addHandler(handle);
     server.on("/", HTTP_POST, handleRequest);
     server.begin();
 }
 
-/* CONTROLADOR DE DISPOSITIVO */
 
-void controlaDispositivo(int porta) {
-    int estado = !retornaEstadoDispositivo(porta);
-    digitalWrite(porta, estado);
-    enviaEstadoDispositivo(200, !estado);
+void setup() {
+
+   /* SERIAL */
+    Serial.begin(115200);
+
+    inicializaWifi();
+
+    serial_hidrico.begin(9200, SERIAL_8N1, 1, 3);
+    serial_eletrico.begin(4800, SERIAL_8N1, 16, 17);
+
+    /* LED ACIONAMENTO */
+    pinMode(LED_ACIONAMENTO, OUTPUT);
+    digitalWrite(LED_ACIONEMENTO, HIGH);
 }
 
-/* STATUS */
+void loop() {
 
-double retornaConsumoEletrico() {
-    return ULTIMA_LEITURA_ELETRICA/1000; //recebeInformacaoSerial("eletrico");
-}
-
-double retornaConsumoHidrico() {
-    return recebeInformacaoSerial("hidrico");
-}
-
-double retornaVolumeReservatorio() {
-    return ((CAPACIDADE_RESERVATORIO / ALTURA_RESERVATORIO) * (ultrasonic.read() * 100) / ALTURA_RESERVATORIO);
-}
-
-int retornaEstadoDispositivo(int porta) {
-    return digitalRead(porta);
-}
-
-/* DECLARACAO */
-
-void declaraReservatorio(double capacidade) {
-    CAPACIDADE_RESERVATORIO = capacidade; // L
-    ALTURA_RESERVATORIO = ultrasonic.read(); // CM
-
-    enviaStatus(200, "RESERVATORIO CONFIGURADO");
-}
-
-
-void declaraEletricidade() {
-  enviaStatus(200, "ELETRICIDADE MONITORAVEL");
-}
-
-void declaraHidricidade() {
-  enviaStatus(200, "ELETRICIDADE MONITORAVEL");
-}
-
-/* GERENCIAMENTO DE PORTAS */
-
-void informaPortaDisponivel() {
-    for (int i = 0; i < 18; i++) {
-        if (portasDisponiveis[i]) {
-            enviaPortaDisponivel(200, portasDisponiveis[i]);
-            return;
-        }
-    }
-
-    enviaStatus(200, "ALCANCE DE LIMITE DE PORTAS UTILIZAVEIS");
-}
-
-void declaracaoDispositivo(int porta) {
-    for (int i = 0; i < 18; i++) {
-        if (portasDisponiveis[i] == porta) {
-            portasDisponiveis[i] = NULL;
-            declaraPorta(porta, OUTPUT);
-            enviaStatus(200, "DISPOSITIVO ADICIONADO");
-            return;
-        }
-    }
-
-    enviaStatus(402, "FALHA NA ADICAO DO DISPOSITIVO");
-}
-
-void declaraPorta(int porta, int fluxo) {
-    pinMode(porta, fluxo);
-
-    if (fluxo == OUTPUT) {
-        digitalWrite(porta, LOW);
-    }
-}
-
-/* RELATORIO */
-
-void enviaRelatorio(double consumo, String unidade) {
-    enviaInformacao(
-        "'consumo':" + String(consumo) + ",'unidade':'" + unidade + "'"
-    );
-}
-
-void enviaStatus(int status, String mensagem) {
-    enviaInformacao(
-        "'status':" + String(status) + ",'mensagem':'" + mensagem + "'"
-    );
-}
-
-void enviaEstadoDispositivo(int status, int estado) {
-    enviaInformacao(
-        "'status':" + String(status) + ",'estado':" + String(estado)
-    );
-}
-
-void enviaPortaDisponivel(int status, int porta) {
-    enviaInformacao(
-        "'status':" + String(status) + ",'porta':" + String(porta)
-    );
 }
