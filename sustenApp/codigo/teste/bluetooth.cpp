7a8179a6ba962ae80ab9085d0a0dd87b160c2a5c
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2023-12-05 21:18:31 -0300

    Update bluetooth.cpp

--- sustenApp/codigo/teste/bluetooth.cpp
+++ sustenApp/codigo/teste/bluetooth.cpp
@@ -1,7 +1,7 @@
 #include "BluetoothSerial.h"
 
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
-# error Bluetooth is not enabled!Please run `make menuconfig`to and enable it
+#error Bluetooth is not enabled!Please run `make menuconfig`to and enable it
 #endif
 
 BluetoothSerial bluetooth;
